05d0305e3734fe71a349dfe111552802d72e9c8a
Maria Luiza <marialuizagon1008@gmail.com>
2023-04-24 11:41:58 -0300

    Instalado os pacotes de conexão com o banco de dados e gestão de usuarios e comentários

---
+++ MaluFlix/Models/AppUser.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace MaluFlix.Models;
+
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [StringLength(60)]
+        public string Name {get; set;}
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth {get; set;}
+        
+        [StringLength(200)]
+        public string ProfilePicture {get; set;}
+
+        
+    }
